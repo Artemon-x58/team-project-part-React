eda0ef3e30efa7f65fc4437becc96205fb0ff786
Artemon-x58 <artem1145819@gmail.com>
2023-12-13 20:42:10 +0100

    create main container

---
+++ src/components/Container/Container.styled.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: blue;
+  z-index: 100;
+  max-width: 320px;
+  padding-left: 10px;
+  padding-right: 10px;
+  margin: 0 auto;
+
+  @media screen and (min-width: 768px) {
+    max-width: 834px;
+    padding-left: 27px;
+    padding-right: 27px;
+  }
+
+  @media screen and (min-width: 1200px) {
+    max-width: 1440px;
+    padding-left: 34px;
+    padding-right: 34px;
+  }
+`;
