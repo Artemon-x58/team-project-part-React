24b96a59385b8fc8c9fc3f0da0edf9390da197f9
taranvd <apple.vlad.taran@gmail.com>
2023-12-21 19:08:48 +0200

    feat: move pages to 'pages' directory and assemble 'MainPage' from components

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,37 @@
+import { Container } from 'components/Container/Container.styled';
+import DailyGoal from 'components/DailyGoal';
+import { DiaryPart } from 'components/Diary/DiaryPart';
+import Food from 'components/Food';
+import Water from 'components/Water';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchAllStatistics } from 'redux/statistics/statisticsOperations';
+import { InfoWrapper, StyledLink, Topic, Wrapper } from './MainPage.styled';
+
+export const MainPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllStatistics());
+  }, [dispatch]);
+
+  return (
+    <>
+      <Container>
+        <Wrapper>
+          <Topic>Today</Topic>
+          <StyledLink>On the way to the goal</StyledLink>
+        </Wrapper>
+        <InfoWrapper>
+          <DailyGoal />
+          <Water />
+          <Food />
+        </InfoWrapper>
+
+        <div>
+          <DiaryPart />
+        </div>
+      </Container>
+    </>
+  );
+};
