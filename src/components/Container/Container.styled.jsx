bbd4fc82f7808f5ce373b1b6c3e71a28837cb12c
Artemon-x58 <artem1145819@gmail.com>
2023-12-24 17:40:49 +0100

    update media

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -7,13 +7,13 @@ export const Container = styled.div`
   margin: 0 auto;
 
   @media screen and (min-width: 834px) {
-    max-width: 834px;
+    max-width: 780px;
     padding-left: 27px;
     padding-right: 27px;
   }
 
   @media screen and (min-width: 1440px) {
-    max-width: 1440px;
+    max-width: 1372px;
     padding-left: 34px;
     padding-right: 34px;
   }
