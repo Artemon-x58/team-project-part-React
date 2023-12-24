2af533dbe0398530d51c4a6f91c8a5d3d5d7a2f5
Vadym-Hryshchuk <82964921+Vadym-Hryshchuk@users.noreply.github.com>
2023-12-24 16:11:50 +0200

    connect recommented pages

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from 'components/Header/Header';
 import { Container } from 'components/Container/Container.styled';
+import { useSelector } from 'react-redux';
+import { selectIsRefreshing } from 'redux/auth/authSelectors';
+import { Loader } from 'components/Loader/Loader';
 
 export const SharedLayout = () => {
+  const isLoading = useSelector(selectIsRefreshing);
   return (
     <>
       <Header />
-      <Container>
-        <Outlet />
-      </Container>
+      <Container>{!isLoading ? <Outlet /> : <Loader />}</Container>
     </>
   );
 };
