e57b73269cc57671a332467c13447c034ad8d044
Artemon-x58 <artem1145819@gmail.com>
2023-12-26 10:54:39 +0100

    test githubPages

--- src/index.jsx
+++ src/index.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { GlobalStyles } from 'components/GlobalStyles/GlobalStyles';
-import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from 'redux/store';
 import { Provider } from 'react-redux';
+
+const router = createHashRouter([
+  {
+    path: '/*',
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyles />
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/team-project-part-React">
-          <App />
-        </BrowserRouter>
+        <RouterProvider router={router} />
       </PersistGate>
     </Provider>
   </React.StrictMode>
