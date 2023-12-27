3c1d6371b4331b3e9f1fc317eb403377b6204e2f
Artemon-x58 <artem1145819@gmail.com>
2023-12-27 17:14:16 +0100

    create BrowserRouter

--- src/index.jsx
+++ src/index.jsx
@@ -2,24 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { GlobalStyles } from 'components/GlobalStyles/GlobalStyles';
-import { RouterProvider, createHashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from 'redux/store';
 import { Provider } from 'react-redux';
-
-const router = createHashRouter([
-  {
-    path: '/*',
-    element: <App />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyles />
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <RouterProvider router={router} />
+        <BrowserRouter basename="https://Artemon-x58.github.io/team-project-part-React">
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
